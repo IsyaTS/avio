e66fc37f814a57650020e4a6b9b1cefe1f1f5c09
IsyaTS <105787976+IsyaTS@users.noreply.github.com>
2025-10-10 22:27:21 +0500

    Handle Telegram auth key failures with soft disconnect

--- app/static/js/connect-tg.js
+++ app/static/js/connect-tg.js
@@ -129,7 +129,7 @@
 
     function updateControls() {
       if (refreshButton) {
-        refreshButton.disabled = startInFlight || inTwoFA;
+        refreshButton.disabled = startInFlight;
       }
     }
 
