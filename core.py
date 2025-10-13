7b4b7c02a1c815d1672e3a29120edd57011f4996
IsyaTS <105787976+IsyaTS@users.noreply.github.com>
2025-10-13 18:34:25 +0500

    Add admin session cookie constant (#89)

--- core.py
+++ core.py
@@ -2,6 +2,8 @@ from app.core import *  # noqa: F401,F403
 
 import importlib as _importlib
 
+ADMIN_COOKIE = "admin_session"
+
 _core_pkg = _importlib.import_module("app.core")
 for _name, _value in _core_pkg.__dict__.items():
     if _name in {"__name__", "__package__", "__spec__", "__loader__", "__file__", "__path__"}:
